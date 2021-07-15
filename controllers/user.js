62d5bef458ba01f4591c2bdaf4e12eb1a34d488b
Shifali <gwal.shifali2223@gamil,com>
2021-07-15 17:04:52 +0530

    Update userInfo

--- controllers/user.js
+++ controllers/user.js
@@ -13,9 +13,39 @@ exports.getUserById = (req, res, next, id) => {
 };
 
 exports.getUser = (req, res) => {
-  // req.profile.salt = undefined;
-  // req.profile.encryptedPassword = undefined;
+  req.profile.salt = undefined;
+  req.profile.encryptedPassword = undefined;
   // req.profile.createdAt = undefined;
   // req.profile.updatedAt = undefined;
   return res.json(req.profile);
+};
+
+// exports.getAllUsers = (req, res) => {
+//   User.find().exec((err, users) => {
+//     if(err || !users){
+//       return res.status(400).json({
+//         error: "No users found"
+//       })
+//     }
+//     res.json(users)
+
+//   })
+// }
+
+exports.updateUser = (req, res) => {
+  User.findByIdAndUpdate(
+    { _id: req.profile._id },
+    { $set: req.body },
+    { new: true, useFindAndModify: false },
+    (err, user) => {
+      if (err) {
+        return res.status(400).json({
+          error: "You are not authorized to update this user",
+        });
+      }
+      user.salt = undefined;
+      user.encryptedPassword = undefined;
+      res.json(user);
+    }
+  );
 };
--- routers/user.js
+++ routers/user.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 //Controllers
-const { getUserById, getUser } = require("../controllers/user");
+const { getUserById, getUser, updateUser } = require("../controllers/user");
 
 const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth");
 
 router.param("userId", getUserById);
 router.get("/user/:userId", isSignedIn, isAuthenticated, getUser);
+// router.get("/users", getAllUsers)
+router.put("/user/:userId", isSignedIn, isAuthenticated, updateUser)
 
 module.exports = router;
