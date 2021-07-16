a884eac590d7e23815b315021a70e591b7bb7da9
Shifali <gwal.shifali2223@gamil,com>
2021-07-16 11:13:55 +0530

    Perform delete operation

--- controllers/category.js
+++ controllers/category.js
@@ -40,16 +40,30 @@ exports.getAllCategory = (req, res) => {
 };
 
 exports.updateCategory = (req, res) => {
-    const category = req.category;
-    category.name = req.body.name;
+  const category = req.category;
+  category.name = req.body.name;
 
-    category.save((err, updatedCategory) => {
-        if (err) {
-            return res.status(400).json({
-              error: "Failed to update category",
-            });
-          }
-          res.json(updatedCategory);
-        });
+  category.save((err, updatedCategory) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Failed to update category",
+      });
+    }
+    res.json(updatedCategory);
+  });
+};
 
-}
+exports.removeCategory = (req, res) => {
+  const category = res.category;
+
+  category.remove((err, category) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Failed to delete this category",
+      });
+    }
+    res.json({
+      message: "Successfull deleted",
+    });
+  });
+};
--- routers/category.js
+++ routers/category.js
@@ -7,6 +7,7 @@ const {
   getCategory,
   getAllCategory,
   updateCategory,
+  removeCategory
 } = require("../controllers/category");
 const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth");
 const { getUserById } = require("../controllers/user");
@@ -37,5 +38,14 @@ router.put(
   isAdmin,
   updateCategory
 );
+
+// delete
+router.delete(
+    "/category/:categoryId/:userId",
+    isSignedIn,
+    isAuthenticated,
+    isAdmin,
+    removeCategory
+  );
 
 module.exports = router;
