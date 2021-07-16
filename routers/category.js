06024d8632c5969e476dd05defc13c2dea8c3a02
Shifali <gwal.shifali2223@gamil,com>
2021-07-16 11:05:41 +0530

    Update category

--- routers/category.js
+++ routers/category.js
@@ -1,27 +1,41 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
 
-const {getCategoryById, 
-    createCategory,
-    getCategory,
-    getAllCategory} = require("../controllers/category")
-const {isSignedIn, isAdmin, isAuthenticated} = require("../controllers/auth")
-const {getUserById} = require("../controllers/user")
-
+const {
+  getCategoryById,
+  createCategory,
+  getCategory,
+  getAllCategory,
+  updateCategory,
+} = require("../controllers/category");
+const { isSignedIn, isAdmin, isAuthenticated } = require("../controllers/auth");
+const { getUserById } = require("../controllers/user");
 
 // params
+// create
 router.param("userId", getUserById);
 router.param("categoryId", getCategoryById);
 
 // actual route
-router.post("/category/create/:userId",
-isSignedIn, 
-isAuthenticated,
-isAdmin ,
-createCategory)
+router.post(
+  "/category/create/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  createCategory
+);
 
+// read
+router.get("/category/categoryId", getCategory);
+router.get("/categories", getAllCategory);
 
-router.get("/category/categoryId", getCategory)
-router.get("/categories", getAllCategory)
+// update
+router.put(
+  "/category/:categoryId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateCategory
+);
 
 module.exports = router;
